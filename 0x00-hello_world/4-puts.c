dd6ec28b4a74c31e9fe540c4238257c42e023cc6
piocharbel17 <piocharbel17@gmail.com>
2023-06-15 10:26:17 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
@@ -8,5 +7,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+return (0);
 }
