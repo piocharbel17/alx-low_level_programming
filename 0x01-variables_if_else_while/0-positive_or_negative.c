0b585274e5067423a640ca8cac564c8445526c5a
piocharbel17 <piocharbel17@gmail.com>
2023-06-16 09:15:43 -0700

    C Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-int n;			
+int n;		
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -24,4 +25,5 @@ else
 printf("%d is negative\n", n);
 }
 return (0);
-}	
+}
+
