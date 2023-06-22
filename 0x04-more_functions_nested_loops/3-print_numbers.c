156db729a8001151936f20e10fdddff74f59bb94
piocharbel17 <piocharbel17@gmail.com>
2023-06-22 06:54:13 -0700

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,13 @@
  * print_numbers - print 0-9
  * return: void
  */
-
 void print_numbers(void)
 {
-        char c;
+	char c;
 
-        for (c = '0'; c <= '9'; c++)
-        {
-                _putchar(c);
-        }
-        _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
